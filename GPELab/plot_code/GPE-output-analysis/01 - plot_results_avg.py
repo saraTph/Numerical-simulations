68730135bd8c56f0bc2480170f55742489676d35
saraTph <sara.tiengo@protonmail.com>
2025-05-14 16:22:47 +0200

    last upd

--- GPELab/plot_code/GPE-output-analysis/01 - plot_results_avg.py
+++ GPELab/plot_code/GPE-output-analysis/01 - plot_results_avg.py
@@ -72,64 +72,100 @@ spin_exp[1,:] = data_spin.get('pop1').squeeze()
 spin_exp[0,:] = data_spin.get('pop0').squeeze()
 
 
-#%%
-colors = plt.get_cmap('Set2').colors
-lw = 1.7
+
+
+
+#%% plots parameters
+
+lw = 1.5
+fs = 14
+ls = 11
+
+size_x = 10
+size_y = 4.5
+
+from matplotlib import cm
+
+# Choose a sequential colormap (e.g., Greens)
+cmap = cm.get_cmap('Greens')
+colors = [cmap(x) for x in np.linspace(0.3, 0.9, 6)]  # Avoid very light and very dark ends
+colors = [cmap(x) for x in np.linspace(1, 0.4, 6)]
+
 labels = [r'$\Omega$ = 30.4 kHz', r'$\Omega$ = 15.2 kHz', r'$\Omega$ = 7.6 kHz', r'$\Omega$ = 3.8 kHz', r'$\Omega$ = 1.9 kHz', r'$\Omega$ = 950 Hz']
-
-# Plotting the expressions
-fig, ax = plt.subplots(1,1,constrained_layout=True, figsize=(9,4))
-
-
-#for i in range(np.size(Omega_values)):
-for i in range(6):
-    Om = Omega_values[i] *2*np.pi
-    
-    ax.plot(delta_values[i,:], pop[i,:], lw=lw, color=colors[i], label=labels[i])
-    ax.scatter(scan_spin, spin_exp[i,:], lw=2, marker = '2', color=colors[i])
-
-
-yticks = np.linspace(0,1,5)
-ytick_labels = [r'$0$', r'$0.25$', r'$0.5$', r'$0.75$' , r'$1$']
-ax.set_yticks(yticks)
-ax.set_xlabel(r'$\delta/\Omega$', fontsize=14)
-ax.set_ylabel(r'$P_{\uparrow\uparrow}$',fontsize = 14)
-ax.legend()
-ax.grid()
-
 
 #%% size after tof
 
-colors = plt.get_cmap('Set2').colors
-lw = 1.7
-labels = [r'$\Omega$ = 30.4 kHz', r'$\Omega$ = 15.2 kHz', r'$\Omega$ = 7.6 kHz', r'$\Omega$ = 3.8 kHz', r'$\Omega$ = 1.9 kHz', r'$\Omega$ = 950 Hz']
-
 from matplotlib.ticker import AutoMinorLocator, MultipleLocator
 
-figS, axS = plt.subplots(1,1,constrained_layout=True, figsize=(10,7))
+fig, ax = plt.subplots(1,2,constrained_layout=True, figsize=(size_x, size_y))
 
 shift = [0.12, 0.122, 0.158, 0.257, 0.257, 0.376]
 #for i in range(np.size(Omega_values)):
 for i in range(0,6):
     Om = Omega_values[i]*2*np.pi
     
-    axS.plot(delta_values[i,:], size[i,:]*10**3, label=labels[i], lw=lw, color=colors[i],zorder =1)
-    axS.scatter(scan_energy + shift[i], size_exp[i,:], lw=2, marker = '2', color=colors[i])
+    ax[0].plot(delta_values[i,:], size[i,:]*10**3, label=labels[i], lw=lw, color=colors[i],zorder =1)
+    ax[0].scatter(scan_energy + shift[i], size_exp[i,:], lw=2, marker = '2', color=colors[i])
 
-xticks = np.linspace(-8,8,17)
-axS.set_xticks(xticks)    
-axS.set_xlabel(r'$\delta/\Omega$', fontsize=14)
-axS.set_ylabel(r'$\sigma$ (mm)',fontsize = 14)  
-axS.set_ylim(0.02,0.25)  
-axS.yaxis.set_major_locator(MultipleLocator(0.05))
-axS.yaxis.set_major_formatter('{x:.2f}')
-axS.yaxis.set_minor_locator(MultipleLocator(0.01))
+xticks = np.linspace(-8,8,5)
+ax[0].set_xticks(xticks)    
+ax[0].tick_params(labelsize=ls)
+ax[0].set_xlabel(r'$\delta/\Omega$', fontsize=fs)
+ax[0].set_ylabel(r'$\sigma(t_{TOF}= 62.6\ ms)$ (mm)',fontsize = fs)  
+ax[0].set_ylim(-0.01,0.25)  
+ax[0].yaxis.set_major_locator(MultipleLocator(0.05))
+ax[0].yaxis.set_major_formatter('{x:.2f}')
+ax[0].yaxis.set_minor_locator(MultipleLocator(0.01))
 #axS.spines['left'].set_position(('data', 0))
 #ax.spines['left'].set_zorder(10)
-axS.legend()
-axS.grid()
+legend = ax[0].legend( 
+    loc='best',
+    fontsize=ls,              # Legend font size
+    frameon=True,            # Show legend box
+    fancybox=False,           # Rounded box corners
+    framealpha=0.9,          # Slight transparency
+    edgecolor='gray',        # Border color
+    facecolor='white'        # Background color
+)
+ax[0].axhline(y=0, color='k', linestyle='--', linewidth=0.8)
+ax[0].axvline(x=0, color='k', linestyle='--', linewidth=0.8)
 
+
+for i in range(6):
+    Om = Omega_values[i] *2*np.pi
+    
+    ax[1].plot(delta_values[i,:], pop[i,:], lw=lw, color=colors[i], label=labels[i])
+    ax[1].scatter(scan_spin, spin_exp[i,:], lw=2, marker = '2', color=colors[i])
+
+
+yticks = np.linspace(0,1,5)
+ytick_labels = [r'$0$', r'$0.25$', r'$0.5$', r'$0.75$' , r'$1$']
+ax[1].set_yticks(yticks)
+ax[1].tick_params(labelsize=ls)
+ax[1].set_xlabel(r'$\delta/\Omega$', fontsize=fs)
+ax[1].set_ylabel(r'$P_{\uparrow}$',fontsize = fs)
+
+ax[1].axhline(y=0, color='k', linestyle='--', linewidth=0.8)
+ax[1].axvline(x=0, color='k', linestyle='--', linewidth=0.8)
+
+
+from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes, mark_inset
+
+
+
+# Create the zoomed-in inset axes
+axins = zoomed_inset_axes(ax[0], zoom=2.5, loc='lower right')  # zoom factor, position
+
+for i in range(0, 6):
+    Om = Omega_values[i]*2*np.pi
+    axins.plot(delta_values[i,:], size[i,:]*10**3, lw=lw, color=colors[i], zorder=1)
+    axins.scatter(scan_energy + shift[i], size_exp[i,:], lw=2, marker='2', color=colors[i])
+
+axins.set_xlim(-1.25, 1.5)   # example range: adjust to your region of interest
+axins.set_ylim(0.02, 0.05)
+axins.set_xticklabels([])
+axins.set_yticklabels([])
+mark_inset(ax[0], axins, loc1=2, loc2=3, fc="none", ec="0.5")
 #%%
 
-#fig.savefig(r'C:\Users\Sarah\Documents\GitHub\Numerical-simulations\GPELab\plot_code\Figures\Spin_DV.png', dpi = 300)
-#figS.savefig(r'C:\Users\Sarah\Documents\GitHub\Numerical-simulations\GPELab\plot_code\Figures\Energy_DV.png', dpi = 300)
+# fig.savefig(r'C:\Users\Sarah\OneDrive\Documenti\InstOptique\Slides and posters\energy&pop.png', dpi = 600)
